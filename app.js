404631229752f514b8ae017fdc4089e7e3219e35
HC <harvey368@hotmail.com>
2017-10-09 16:22:45 -0400

    First try out code

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// First code
+// First try out code
+var budgetController = ( function () {       //--> IIFE function
+     var x=23;
+     var add= function (a) {
+         return x + a;
+     }
+
+     return {    // return a object. and object contain a method publicTest
+         publicTest: function(b){      // Not return a function but a object
+             console.log(b);
+         }
+     }
+
+    }
+)();

0132f2b6e511b6f60f6bd5b56face81a97c42b3b
HC <harvey368@hotmail.com>
2017-10-09 23:22:37 -0400

    Section 6-5 Add Event listener

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var UIController=(function () {
 //----------------------Global APP controller ----------------
 var controller = (function (budgetCtrl, UICtrl) { //Bridge of 2 controller
 
-    document.querySelector('.add_btn').addEventListener('click',function () {
+    document.querySelector('.add__btn').addEventListener('click',function () {
         //1. Get the filed input date
 
         //2. Add the item to the budget controller
@@ -26,11 +26,12 @@ var controller = (function (budgetCtrl, UICtrl) { //Bridge of 2 controller
         //4. Calculate the budget
 
         //5. Display the budget on the UI
-    })
+    });
 
     document.addEventListener('keypress',function(event) { //check every key press event
         console.log(event);                  // Browser will pass object (keypress) to variable
-    })
+        console.log(event.keyCode);        // Enter is 13
+    });
 
 })(budgetController, UIController);
 

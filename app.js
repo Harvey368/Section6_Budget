052653dbeb0826fe3bc3d14c65b921ab4ef1220b
harvey.chen <harvey368@hotmail.com>
2017-10-11 13:08:52 -0400

    Section 6 - 6 1.Create getInput method to return data to appcontroller

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ var budgetController = ( function () {       //--> IIFE function
 
 
 //---------------------- UI Controller --------------------
-var UIController=(function () {
-    
+var UIController=(function () {   // Since it is IIFE, so we need return a object to UIcontroller
+
+    var DOMStrings={  //Create a interface to store all outside info (UI) which maybe changed frequently
+        inputType:  '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
+
+    return {      //Return a object, it own a method to get the input data
+        getInput: function () {
+            return {               // This method should return a object include all input data
+                type: document.querySelector(DOMStrings.inputType).value,   //Will be either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        }
+
+    };
+
 })();
 
 
@@ -17,7 +34,10 @@ var UIController=(function () {
 var controller = (function (budgetCtrl, UICtrl) { //Bridge of 2 controller
 
     var ctrlAddItem=function () {
+
         //1. Get the filed input date
+        var input= UICtrl.getInput();
+        console.log(input);
 
         //2. Add the item to the budget controller
 
@@ -26,7 +46,7 @@ var controller = (function (budgetCtrl, UICtrl) { //Bridge of 2 controller
         //4. Calculate the budget
 
         //5. Display the budget on the UI
-        console.log('it works!')
+
     }
 
 

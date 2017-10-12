3f675c2ec451b799a283f4304778665da1e99be0
harvey.chen <harvey368@hotmail.com>
2017-10-12 16:28:26 -0400

    Section 6 - 10 2.Insert the HTML string into DOM and call it via app controller

--- app.js
+++ app.js
@@ -68,7 +68,9 @@ var UIController=(function () {   // Since it is IIFE, so we need return a objec
         inputType:  '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     }
 
     return {      //Return a object, it own a method to get the input data
@@ -81,22 +83,24 @@ var UIController=(function () {   // Since it is IIFE, so we need return a objec
         },
 
         addListItem: function (obj, type) {
-            var html, newHtml;
+            var html, newHtml, element;
 
             //Create HTML string with placeholder text
             if (type === 'inc') {
+                element = DOMStrings.incomeContainer;
                 html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
+                element = DOMStrings.expenseContainer;
                 html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
             // Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
+            newHtml = newHtml.replace('%value%', obj.value);
 
             // Insert the HTML into the DOM
-
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml );  //Add the item in HTML file
         },
         
         getDOMstrings: function () {  //This method allow other module can share this info outside of it
@@ -135,6 +139,7 @@ var controller = (function (budgetCtrl, UICtrl) { //Bridge of 2 controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3. Add the item to UI
+        UICtrl.addListItem(newItem, input.type);
 
         //4. Calculate the budget
 
